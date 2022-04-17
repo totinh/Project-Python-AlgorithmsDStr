8b58b4663c57cfebb87acf7e935268dc83022fa5
Tịnh Tô <totinh>
2022-04-18 01:25:33 +0700

    commit 18/4/2022

--- 5.TicTacToe/game.py
+++ 5.TicTacToe/game.py
@@ -1,3 +1,11 @@
+# from tempfile import SpooledTemporaryFile
+# from tkinter import dialog
+
+import time
+from unittest import result
+from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
+
+
 class TicTacToe:
         def __init__(self):
             self.board = [' ' for _ in range(9)] # we will use a single list to rep 3x3 board
@@ -45,6 +53,28 @@ class TicTacToe:
             # first let's check the row
             row_ind = square // 3
             row = self.board[row_ind*3 : (row_ind + 1) * 3]
+            if all([spot == letter for spot in row]):
+                return True
+
+            # check column
+            col_ind = square % 3
+            column = [self.board[col_ind+i*3] for i in range(3)]
+            if all([spot == letter for spot in column]):
+                return True
+
+            # check diagonals
+            # but only if the square is an even number (0,2,4,6,8)
+            # these are the only moves possible to win a diagonal
+            if square % 2 == 0:
+                diagonal1 = [self.board[i] for i in [0, 4, 8]] #left to right diagonal
+                if all([spot == letter for spot in diagonal1]):
+                    return True
+                diagonal2 = [self.board[i] for i in [2, 4, 6]] #right to left diagonal
+                if all([spot == letter for spot in diagonal2]):
+                    return True
+
+            # if all of these fail
+            return False
 
 def play(game, x_player, o_player, print_game=True):
     # returns the winner of the game(the letter)! or None for a tie
@@ -57,7 +87,7 @@ def play(game, x_player, o_player, print_game=True):
 
     while game.empty_squares():
         # get the move  from the appropriate player
-        if letter == '0':
+        if letter == 'O':
             square = o_player.get_move(game)
         else:
             square = x_player.get_move(game)
@@ -81,6 +111,31 @@ def play(game, x_player, o_player, print_game=True):
             # else:
             #     letter = 'X'
 
+        # tiny break to make things a little easier to read
         if print_game:
-            print('It\'s a tie!')
+            time.sleep(0.8)
+
+    if print_game:
+        print('It\'s a tie!')
+
+if __name__ == '__main__':
+    # x_player = HumanPlayer('X')
+    # o_player = GeniusComputerPlayer('O')
+    # t = TicTacToe()
+    # play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(100):
+        x_player = RandomComputerPlayer('X')
+        o_player = GeniusComputerPlayer('O')
+        t = TicTacToe()
+        result =  play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+    print(f'After 1000 iterations, we see {x_wins} X wins, {o_wins} O wins, and {ties} ties')
 
